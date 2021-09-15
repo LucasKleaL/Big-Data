ef22fde995c8ba46a0a823c43b4c62d814230984
= <=>
2021-09-14 21:31:46 -0300

    Classe final exercicio 6 (HighestPrice)

--- src/TDE/HighestPrice.java
+++ src/TDE/HighestPrice.java
@@ -1,10 +1,9 @@
 package TDE;
 
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -14,99 +13,76 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.log4j.BasicConfigurator;
 
+import java.io.IOException;
+
 public class HighestPrice {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
+
         BasicConfigurator.configure();
 
         Configuration c = new Configuration();
+
         String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
 
-        //  arquivo de entrada
+        //  Arquivo de entrada
         Path input = new Path(files[0]);
 
-        //  arquivo de saida
+        //  Arquivo de saida
         Path output = new Path(files[1]);
 
-        //  criacao do job e seu nome
-        Job j = new Job(c, "highestPrice");
+        //  Criacao do job
+        Job j = new Job(c, "HighestPrice");
 
-        //  registro de classes
+        //  Registro das classes
         j.setJarByClass(HighestPrice.class);
-        j.setMapperClass(HighestPrice.HighestPriceMapper.class);
-        j.setReducerClass(HighestPrice.HighestPriceReducer.class);
+        j.setMapperClass(MapForHighestPrice.class);
+        j.setReducerClass(ReduceForHighestPrice.class);
 
-        //  definição dos tipos de saida
-        //  map
+        //  Map
         j.setMapOutputKeyClass(Text.class);
         j.setMapOutputValueClass(DoubleWritable.class);
 
-        //  reduce
+        //  Reduce
         j.setOutputKeyClass(Text.class);
         j.setOutputValueClass(DoubleWritable.class);
 
-        //  definição dos tipos de entrada e saida
         FileInputFormat.addInputPath(j, input);
         FileOutputFormat.setOutputPath(j, output);
 
         System.exit(j.waitForCompletion(true) ? 0 : 1);
     }
 
-    public class HighestPriceMapper extends Mapper<Object, Text, Text, DoubleWritable> {
-        public void map(Object key, Text value, Context con) throws IOException,
-                InterruptedException {
+    public static class MapForHighestPrice extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+        public void map(LongWritable key, Text value, Context con)
+                throws IOException, InterruptedException {
 
-            //  Obtendo o valor da linha
-            String content = value.toString();
+            String linha = value.toString();
 
-            if (content.startsWith("country_or_area")) {
-                return;
-            }
+            if (linha.startsWith("country_or_area")) return;
 
-            //  Quebrando a linha em campos
-            String contentSplit[] = content.split(";");
+            String[] campos = linha.split(";");
 
-            String keyValue = contentSplit[3] + " " + contentSplit[1];
-
-            double price = Double.parseDouble(contentSplit[5]);
+            String keyValue = campos[1] + " " + campos[3];
+            double price = Double.parseDouble(campos[5]);
 
             con.write(new Text(keyValue), new DoubleWritable(price));
-
-            /*
-            //  Acessando a posição 1 (ano)
-            String year = contentSplit[1];
-
-            //  Acessando a posição 3 (commodity)
-            String commodity = contentSplit[3];
-
-            //  Acessando a posição 5 (preço)
-            double price = Double.parseDouble(contentSplit[5]);
-
-            //  Emitindo os parametros do contexto
-            con.write(new Text(year), new HighestPriceWritable(commodity, price));
-             */
-
         }
     }
 
-    public class HighestPriceReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+    public static class ReduceForHighestPrice extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+        public void reduce(Text key, Iterable<DoubleWritable> values, Context con)
+                throws IOException, InterruptedException {
 
-        public void reduce(Text key,
-                           Iterable<DoubleWritable> values,
-                           Context con) throws IOException, InterruptedException {
-
-            String commodity = "";
             double maxPrice = Double.MIN_VALUE;
 
-            for (DoubleWritable v : values) {
-                if (v.get() > maxPrice) {
-                    maxPrice = v.get();
-                }
+            for(DoubleWritable o : values){
+                if (o.get()  > maxPrice)
+                    maxPrice = o.get();
             }
 
+            // escrevendo os maiores valores em arquivo
             con.write(key, new DoubleWritable(maxPrice));
-
         }
     }
-
 }
